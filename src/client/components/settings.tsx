c304c2e85657c9e613d9e93bae9131bdf2295f8b
defeten <defeten@rsmtp.com>
2025-10-02 03:01:27 -0400

    feat: improve settings/preferences ui

--- src/client/components/settings.tsx
+++ src/client/components/settings.tsx
@@ -1,4 +1,5 @@
 import { useLocalStorage } from "@uidotdev/usehooks";
+import { Switch } from "radix-ui";
 import type {
   LinkBehavior,
   MaxLinkLength,
@@ -23,67 +24,86 @@ export function Settings() {
     useLocalStorage<UnfocusInputAfterSend>("pref:unfocusaftersend", false);
 
   return (
-    <div className="mx-auto my-auto grid grid-cols-2 justify-center rounded-md bg-stone-700 p-2 text-stone-300">
-      <p className="col-span-2 mx-auto text-center text-lg font-bold">
-        Preferences
-      </p>
-      <label className="text-sm font-semibold" htmlFor="showtime">
-        Show time labels
-      </label>
-      <input
-        id="showtime"
-        type="checkbox"
-        className="mr-auto"
-        checked={time}
-        onChange={({ target }) => setTime(target.checked)}
-      />
-      <label className="text-sm font-semibold" htmlFor="scrollbehavior">
-        Scroll behavior
-      </label>
-      <select
-        id="scrollbehavior"
-        className="outline-none"
-        value={scrollBehavior}
-        onChange={({ target }) =>
-          setScrollBehavior(target.value as ScrollBehavior)
-        }
-      >
-        <option value={"instant" satisfies ScrollBehavior}>instant</option>
-        <option value={"smooth" satisfies ScrollBehavior}>smooth</option>
-      </select>
-      <label className="text-sm font-semibold" htmlFor="maxlinklength">
-        Max link length
-      </label>
-      <input
-        type="number"
-        min={30}
-        id="maxlinklength"
-        className="leading-3 outline-none"
-        value={maxLinkLength}
-        onChange={({ target }) => setMaxLinkLength(Number(target.value))}
-      />
-      <label className="text-sm font-semibold" htmlFor="linkbehavior">
-        Link behavior
-      </label>
-      <select
-        id="linkbehavior"
-        className="outline-none"
-        value={linkBehavior}
-        onChange={({ target }) => setLinkBehavior(target.value as LinkBehavior)}
-      >
-        <option value={"newtab" satisfies LinkBehavior}>newtab</option>
-        <option value={"copy" satisfies LinkBehavior}>copy</option>
-      </select>
-      <label className="text-sm font-semibold" htmlFor="unfocusaftersend">
-        Sending closes keyboard
-      </label>
-      <input
-        type="checkbox"
-        id="unfocusaftersend"
-        className="mr-auto"
-        checked={unfocusInput}
-        onChange={({ target }) => setUnfocusInput(target.checked)}
-      />
+    <div className="mx-auto flex w-full grow flex-col justify-center gap-y-4 rounded-md p-2 text-stone-300 md:w-1/2">
+      <p className="mx-auto text-center text-lg font-bold">Preferences</p>
+      <div className="flex items-center justify-between">
+        <label className="text-sm font-semibold" htmlFor="showtime">
+          Show time labels
+        </label>
+        <Switch.Root
+          id="showtime"
+          onChange={({ currentTarget }) =>
+            setTime(currentTarget.dataset.state === "checked")
+          }
+          className="relative h-6 w-10 cursor-default rounded-full bg-stone-800 outline-none data-[state=checked]:bg-emerald-800"
+        >
+          <Switch.Thumb className="shadow-blackA4 block size-5 translate-x-0.5 rounded-full bg-stone-400 transition-transform duration-200 will-change-transform data-[state=checked]:translate-x-4.5 data-[state=checked]:bg-stone-200" />
+        </Switch.Root>
+      </div>
+      <div className="flex items-center justify-between">
+        <label className="text-sm font-semibold" htmlFor="scrollbehavior">
+          Scroll behavior
+        </label>
+        <select
+          id="scrollbehavior"
+          className="outline-none"
+          value={scrollBehavior}
+          onChange={({ target }) =>
+            setScrollBehavior(target.value as ScrollBehavior)
+          }
+        >
+          <option value={"instant" satisfies ScrollBehavior}>instant</option>
+          <option value={"smooth" satisfies ScrollBehavior}>smooth</option>
+        </select>
+      </div>
+      <div className="flex items-center justify-between">
+        <label className="text-sm font-semibold" htmlFor="maxlinklength">
+          Max link length (chars)
+        </label>
+        <input
+          type="text"
+          min={30}
+          id="maxlinklength"
+          pattern="\d*"
+          className="w-min border-b border-stone-200 leading-3 outline-none"
+          value={maxLinkLength}
+          onFocus={({ target }) => (target.value = "")}
+          onBlur={({ target }) => {
+            if (target.value == "") target.value = maxLinkLength.toString();
+          }}
+          onChange={({ target }) => setMaxLinkLength(Number(target.value))}
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <label className="text-sm font-semibold" htmlFor="linkbehavior">
+          Link behavior
+        </label>
+        <select
+          id="linkbehavior"
+          className="outline-none"
+          value={linkBehavior}
+          onChange={({ target }) =>
+            setLinkBehavior(target.value as LinkBehavior)
+          }
+        >
+          <option value={"newtab" satisfies LinkBehavior}>newtab</option>
+          <option value={"copy" satisfies LinkBehavior}>copy</option>
+        </select>
+      </div>
+      <div className="flex items-center justify-between">
+        <label className="text-sm font-semibold" htmlFor="unfocusaftersend">
+          Sending closes keyboard
+        </label>
+        <Switch.Root
+          id="unfocusaftersend"
+          onChange={({ currentTarget }) =>
+            setUnfocusInput(currentTarget.dataset.state === "checked")
+          }
+          className="relative h-6 w-10 cursor-default rounded-full bg-stone-800 outline-none data-[state=checked]:bg-emerald-800"
+        >
+          <Switch.Thumb className="shadow-blackA4 block size-5 translate-x-0.5 rounded-full bg-stone-400 transition-transform duration-200 will-change-transform data-[state=checked]:translate-x-4.5 data-[state=checked]:bg-stone-200" />
+        </Switch.Root>
+      </div>
     </div>
   );
 }

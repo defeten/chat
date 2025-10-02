81a3e0e2fe8a6d90319b3071f0f2f569e6aff6da
defeten <defeten@rsmtp.com>
2025-10-02 04:03:21 -0400

    fix: use strictmode in development

--- src/client/frontend.tsx
+++ src/client/frontend.tsx
@@ -1,16 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "@/client/App";
+import _App from "@/client/App";
 import "@/client/emotes.css";
 import "@/client/index.css";
 
 const elem = document.getElementById("root")!;
 
+const App =
+  process.env.NODE_ENV === "production" ? (
+    <_App />
+  ) : (
+    <StrictMode>
+      <_App />
+    </StrictMode>
+  );
+
 if (import.meta.hot) {
   // With hot module reloading, `import.meta.hot.data` is persisted.
   const root = (import.meta.hot.data.root ??= createRoot(elem));
-  root.render(<App />);
+  root.render(App);
 } else {
   // The hot module reloading API is not available in production.
-  createRoot(elem).render(<App />);
+  createRoot(elem).render(App);
 }
